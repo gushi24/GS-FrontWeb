7a9d41f3d53bb31af89ac11b6bbd34d262121c33
Gushi <mgmorioka@outlook.com>
2024-11-12 04:14:37 -0300

    Creating Routes on main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import Error from './routes/Error.jsx'
+import Home from './routes/Home.jsx'
+import Login from './routes/Login.jsx'
+import Sobre from './routes/Sobre.jsx'
+import Solução from './routes/Solução.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:'/',element:<App/>,
+    errorElement:<Error/>,
+
+    children:[
+      {path:'/',element:<Home/>},
+      {path:'login',element:<Login/>},
+      {path:'sobre',element:<Sobre/>},
+      {path:'solução',element:<Solução/>},
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
